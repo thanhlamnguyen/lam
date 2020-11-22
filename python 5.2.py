14a3898d10451447e4e1171f003e97c02c6004ff
thanhlamnguyen <73697661+thanhlamnguyen@users.noreply.github.com>
2020-11-22 15:20:03 +0700

    Add files via upload

--- python 5.2.py
+++ python 5.2.py
@@ -7,12 +7,17 @@ Created on Thu Nov 19 17:56:43 2020
 
 import numpy as np
 n = int(input("Số các giá trị các phần tử ="))
-rd = np.random.random_sample(size = n)
+rd = np.random.random_sample(size = n)*30000000-20000000
 n = 1
 print ("Giá trị các phần tử =",rd)
 import math
 for x in rd:
     print ("Giá trị các phần tử thứ ",n,"=",x)
     n=n+1
-    print ("Logarith của các phần tử thứ",n,"=",math.log(x))
+    if x > 0:
+         print ("Logarith của các phần tử thứ",n,"=",math.log(x))
+    else:
+        print ("Không có logarith")
+        
+   
 
