192bbabeec93266b8ae757959b85b6ce324c4906
thanhlamnguyen <73697661+thanhlamnguyen@users.noreply.github.com>
2021-01-06 15:25:51 +0700

    Update bài 13.3(chương 4).py

--- bài 13.3(chương 4).py
+++ bài 13.3(chương 4).py
@@ -2,27 +2,31 @@
 
 import random
 import string
-#13.1.1: Nhập dung lượng dữ liệu giới hạn là  1MB <= size <= 1024MB.
-dulieu=int(input('nhập giới hạn dữ liệu(MB)= '))
-while (dulieu<1 or dulieu>1024):
-	dulieu = int(input('nhập lại giới hạn dữ liệu (1->1024MB)= '))
-#13.1.2: Giới hạn dung lượng mỗi file là  1000KB.
-t=1000*1024 
-a= dulieu*(2**20)//(t) 
-b= dulieu*(2**20)%(t)
-#13.3.3: Hãy sinh dữ liệu ngẫu nhiên kiểu chuỗi và lưu vào file
-f= string.ascii_lowercase*48733
+
+
 def text(): 
     c=''.join(random.sample(f,t))
     return c
-print('số file có kích thước 1000KB là: ',a)
-print('file cuối cùng có kích thước: ',b/1024,"KB")
+
+
+dulieu = int(input('nhập giới hạn dữ liệu(MB)= '))
+while (dulieu<1 or dulieu>1024):
+	dulieu = int(input('nhập lại giới hạn dữ liệu (1->1024MB)= '))
+t = 1000*1024 
+a = dulieu*(2**20)//(t) 
+b = dulieu*(2**20)%(t)
+f = string.ascii_lowercase*48733
+def text(): 
+    c=''.join(random.sample(f,t))
+    return c
+print('số file có kích thước 1000KB là: ', a)
+print('file cuối cùng có kích thước: ', b/1024,"KB")
 for i in range(a): 
-	file=open('oejb'+str(i+1)+'.txt' ,'x')
+	file = open('oejb'+str(i+1)+'.txt' ,'x')
 	file.write(text())
 	file.close()
-if b>0: 
-    file=open('pêns'+'.txt' ,'x')
+if b > 0: 
+    file = open('pêns'+'.txt' ,'x')
     file.write(''.join(random.sample(f,b)))
 file.close()
 print('Kết thúc')
